88405761eb4a74990747f73d4e3584bd33ddd144
Vera Clemens <clemens@zbmed.de>
2025-05-28 15:58:23 +0200

    feat: split "LinkDataset/Dataverse" permission off of "PublishDataset/Dataverse"

--- src/main/java/edu/harvard/iq/dataverse/engine/command/impl/LinkDatasetCommand.java
+++ src/main/java/edu/harvard/iq/dataverse/engine/command/impl/LinkDatasetCommand.java
@@ -29,7 +29,7 @@ import org.apache.solr.client.solrj.SolrServerException;
  *
  * @author skraffmiller
  */
-@RequiredPermissions(Permission.PublishDataset)
+@RequiredPermissions(Permission.LinkDataset)
 public class LinkDatasetCommand extends AbstractCommand<DatasetLinkingDataverse> {
     
     private final Dataset linkedDataset;
