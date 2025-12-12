eb47ddb0a88da94995d9312a2993c0a44661acc7
Vera Clemens <clemens@zbmed.de>
2025-12-12 14:08:24 +0100

    feat: add API for getting default contributor role of a collection

--- src/main/java/edu/harvard/iq/dataverse/api/Dataverses.java
+++ src/main/java/edu/harvard/iq/dataverse/api/Dataverses.java
@@ -1636,6 +1636,21 @@ public class Dataverses extends AbstractApiBean {
 
     }
 
+    @GET
+    @AuthRequired
+    @Path("{identifier}/defaultContributorRole")
+    public Response getDefaultContributorRole(
+            @Context ContainerRequestContext crc,
+            @PathParam("identifier") String dvIdtf) {
+
+        return response(req -> ok(
+                json(
+                        execCommand(
+                                new GetCollectionDefaultContributorRoleCommand(req,
+                                        findDataverseOrDie(dvIdtf))))), getRequestUser(crc));
+
+    }
+
     @DELETE
     @AuthRequired
     @Path("{identifier}/groups/{aliasInOwner}")
