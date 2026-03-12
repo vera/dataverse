fa2d8a5f3c744671d594cba66b0ca4fb57da1d87
Vera Clemens <clemens@zbmed.de>
2026-03-12 16:26:56 +0100

    fix: fix bug in API for replacing dataset relations

--- src/main/java/edu/harvard/iq/dataverse/dataset/DatasetRelationServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/dataset/DatasetRelationServiceBean.java
@@ -8,15 +8,17 @@ package edu.harvard.iq.dataverse.dataset;
 import edu.harvard.iq.dataverse.Dataset;
 
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
+import jakarta.ejb.EJB;
 import jakarta.ejb.Stateless;
 import jakarta.ejb.TransactionAttribute;
 import jakarta.ejb.TransactionAttributeType;
 import jakarta.inject.Named;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.TypedQuery;
 
 /**
  *
@@ -28,10 +30,19 @@ public class DatasetRelationServiceBean {
 
     private static final Logger logger = Logger.getLogger(DatasetRelationServiceBean.class.getCanonicalName());
 
-    @PersistenceContext(unitName = "VDCNet-ejbPU")
+    @PersistenceContext
     private EntityManager em;
 
-    public void deleteDatasetRelationsFor(Dataset d) {
+    /**
+     * A reference to the current instance of the DatasetRelationServiceBean.
+     * Used when self-invocation is required for internal method calls
+     * within the same bean to ensure that all EJB functionalities
+     * such as transactions and security are properly applied.
+     */
+    @EJB
+    private DatasetRelationServiceBean self;
+
+    public void deleteAllDatasetRelationsFor(Dataset d) {
         em.createNamedQuery("DatasetRelation.removeRelationsByDatasetId")
                 .setParameter("datasetId", d.getId())
                 .executeUpdate();
@@ -66,12 +77,51 @@ public class DatasetRelationServiceBean {
                 .getSingleResult();
     }
 
-    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public List<DatasetRelation> addDatasetRelations(List<DatasetRelation> relations) {
         for (DatasetRelation relation : relations) {
             em.persist(relation);
         }
         return relations;
+    }
+
+    public List<DatasetRelation> replaceAllDatasetRelationsFor(Dataset d, List<DatasetRelation> newRelations) {
+        // Execute the update (in one atomic operation using a transaction)
+        // Note: We need to call via self-reference so the EJB container can create a transaction as intended.
+        return self.replaceAll(d, newRelations);
+    }
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+    public List<DatasetRelation> replaceAll(Dataset d, List<DatasetRelation> newRelations) {
+        List<DatasetRelation> existingRelations = em.createNamedQuery("DatasetRelation.getRelationsDefinedAtDatasetId", DatasetRelation.class)
+                .setParameter("datasetId", d.getId())
+                .getResultList();
+
+        Set<String> existingKeys = existingRelations.stream()
+                .map(DatasetRelation::toKey)
+                .collect(Collectors.toSet());
+        Set<String> newKeys = newRelations.stream()
+                .map(DatasetRelation::toKey)
+                .collect(Collectors.toSet());
+
+        List<DatasetRelation> toAdd = newRelations.stream()
+                .filter(r -> !existingKeys.contains(r.toKey()))
+                .toList();
+        List<DatasetRelation> toRemove = existingRelations.stream()
+                .filter(r -> !newKeys.contains(r.toKey()))
+                .toList();
+
+        if (!toRemove.isEmpty()) {
+            em.createQuery("DELETE FROM DatasetRelation dr WHERE dr IN :toRemove")
+                    .setParameter("toRemove", toRemove)
+                    .executeUpdate();
+        }
+        for (DatasetRelation r : toAdd) {
+            em.persist(r);
+        }
+
+        em.flush();
+
+        return newRelations;
     }
 
 }
