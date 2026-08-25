e794e40ad94854c18be3df0fed24afcbc6a11194
Vera Clemens <clemens@zbmed.de>
2026-08-25 14:54:01 +0200

    feat: refactor dataset relation queries for improved performance

--- src/main/java/edu/harvard/iq/dataverse/dataset/SqlDirectDatasetRelationAlgorithm.java
+++ src/main/java/edu/harvard/iq/dataverse/dataset/SqlDirectDatasetRelationAlgorithm.java
@@ -16,12 +16,52 @@ import java.util.stream.Collectors;
 @ApplicationScoped
 public class SqlDirectDatasetRelationAlgorithm implements DatasetRelationAlgorithm {
 
-//    private static final Logger logger = Logger.getLogger(SqlDirectDatasetRelationAlgorithm.class.getCanonicalName());
-
     @PersistenceContext
     private EntityManager em;
 
-    private static final String GET_TOTAL_RELATION_COUNT_QUERY_BASE = 
+    private static final String WITH_LATEST_RELEASED_VERSIONS =
+            " WITH latest_released_versions AS MATERIALIZED ( " +
+            "     SELECT DISTINCT ON (dataset_id) id, dataset_id " +
+            "     FROM datasetversion " +
+            "     WHERE versionstate = 'RELEASED' " +
+            "     ORDER BY dataset_id, id DESC " +
+            " ), ";
+
+    private static final String WITH_CANDIDATE_RELATIONS =
+            " candidate_relations AS ( " +
+            // Relations defined on the requested version.
+            "     SELECT dr.id, 0 AS definition_point_priority " +
+            "     FROM datasetrelation dr " +
+            "     WHERE dr.definitionpoint_id = ? " +
+            "     UNION ALL " +
+            // Incoming relations defined on the latest released version of another dataset.
+            "     SELECT dr.id, 1 AS definition_point_priority " +
+            "     FROM datasetrelation dr " +
+            "     JOIN latest_released_versions lrv ON dr.definitionpoint_id = lrv.id " +
+            "     WHERE lrv.dataset_id != ? " +
+            "       AND (dr.dataset_id = ? OR dr.relateddataset_id = ?) " +
+            " ) ";
+
+    private static final String WITH_CANONICAL_RELATIONS =
+            " , normalized_candidate_relations AS ( " +
+            "     SELECT cr.id, cr.definition_point_priority, " +
+            "         CASE WHEN dr.dataset_id = ? THEN dr.relationtype_id ELSE rt.inverse_id END AS normalized_relation_type_id, " +
+            "         CASE WHEN dr.relation_source = 'internal' " +
+            "             THEN CASE WHEN dr.dataset_id = ? THEN CAST(dr.relateddataset_id AS VARCHAR) ELSE CAST(dr.dataset_id AS VARCHAR) END " +
+            "             ELSE dr.externalidentifier " +
+            "         END AS normalized_related_dataset " +
+            "     FROM candidate_relations cr " +
+            "     JOIN datasetrelation dr ON cr.id = dr.id " +
+            "     LEFT JOIN datasetrelationtype rt ON dr.relationtype_id = rt.id " +
+            " ), " +
+            " canonical_relations AS ( " +
+            "     SELECT DISTINCT ON (normalized_relation_type_id, normalized_related_dataset) " +
+            "         id, definition_point_priority " +
+            "     FROM normalized_candidate_relations " +
+            "     ORDER BY normalized_relation_type_id, normalized_related_dataset, definition_point_priority, id " +
+            " ) ";
+
+    private static final String GET_TOTAL_RELATION_COUNT_QUERY_BASE =
             // Count the number of unique related datasets
             " SELECT COUNT(DISTINCT " +
             "    CASE " +
@@ -29,34 +69,18 @@ public class SqlDirectDatasetRelationAlgorithm implements DatasetRelationAlgorit
             "            CASE WHEN dr.dataset_id = ? THEN CAST(dr.relateddataset_id AS VARCHAR) ELSE CAST(dr.dataset_id AS VARCHAR) END " +
             "        ELSE dr.externalidentifier " +
             "    END) " +
-            " FROM datasetrelation dr " +
-            " JOIN datasetversion dv_def ON dr.definitionpoint_id = dv_def.id ";
+            " FROM candidate_relations cr " +
+            " JOIN datasetrelation dr ON cr.id = dr.id ";
 
-    private static final String GET_RELATIONS_QUERY_BASE = 
+    private static final String GET_RELATIONS_QUERY_BASE =
             " SELECT dr.* " +
-            " FROM datasetrelation dr " +
-            " JOIN datasetversion dv_def ON dr.definitionpoint_id = dv_def.id ";
+            " FROM canonical_relations cr " +
+            " JOIN datasetrelation dr ON cr.id = dr.id ";
 
     private static final String JOIN_RELATION_TYPES = 
             // Get information about dataset relation types
             " JOIN datasetrelationtype rt ON dr.relationtype_id = rt.id " +
             " LEFT JOIN datasetrelationtype inv ON rt.inverse_id = inv.id ";
-
-    private static final String WHERE_DATASET_OR_DATASET_VERSION_MATCHES = 
-            " ( " +
-            // The relation must either be defined on the given dataset version...
-            "    (dr.definitionpoint_id = ?) " +
-            // ...or, even if it's not defined on the given dataset version, the relation might still be relevant if it involves the given dataset and is defined on the latest released version of another dataset
-            "    OR ( " +
-            "        dv_def.dataset_id != ? " +
-            "        AND (dr.dataset_id = ? OR dr.relateddataset_id = ?) " +
-            "        AND dr.definitionpoint_id = ( " +
-            "            SELECT dv.id FROM datasetversion dv " +
-            "            WHERE dv.dataset_id = dv_def.dataset_id " +
-            "            AND dv.id = (SELECT MAX(dv2.id) FROM datasetversion dv2 WHERE dv2.dataset_id = dv.dataset_id AND dv2.versionstate = 'RELEASED') " +
-            "        ) " +
-            "    ) " +
-            " ) ";
 
     private static final String WHERE_RELATION_TYPE_MATCHES = 
             // The relation type must match the given one (inverted if necessary)
@@ -64,40 +88,6 @@ public class SqlDirectDatasetRelationAlgorithm implements DatasetRelationAlgorit
             "    (dr.dataset_id = ? AND rt.name IN (?)) " +
             "    OR " +
             "    (dr.relateddataset_id = ? AND inv.name IN (?)) " +
-            " ) ";
-
-    private static final String WHERE_RELATION_IS_NOT_A_DUPLICATE = 
-            " dr.id = ( " +
-            "    SELECT dr2.id FROM datasetrelation dr2 " +
-            "    JOIN datasetversion dv_def2 ON dr2.definitionpoint_id = dv_def2.id " +
-            "    WHERE ( " +
-            "        dr2.definitionpoint_id = ? " +
-            "        OR ( " +
-            "            (dr2.dataset_id = ? OR (dr2.relation_source = 'internal' AND dr2.relateddataset_id = ?)) " +
-            "            AND dv_def2.dataset_id != ? " +
-            "            AND dr2.definitionpoint_id = ( " +
-            "                SELECT dv2.id FROM datasetversion dv2 " +
-            "                WHERE dv2.dataset_id = dv_def2.dataset_id " +
-            "                AND dv2.id = (SELECT MAX(dv3.id) FROM datasetversion dv3 WHERE dv3.dataset_id = dv2.dataset_id AND dv3.versionstate = 'RELEASED') " +
-            "            ) " +
-            "        ) " +
-            "      ) " +
-            "      AND ( " +
-            "        CASE WHEN dr.dataset_id = ? THEN dr.relationtype_id ELSE (SELECT rt.inverse_id FROM datasetrelationtype rt WHERE rt.id = dr.relationtype_id) END " +
-            "        = " +
-            "        CASE WHEN dr2.dataset_id = ? THEN dr2.relationtype_id ELSE (SELECT rt2.inverse_id FROM datasetrelationtype rt2 WHERE rt2.id = dr2.relationtype_id) END " +
-            "      ) " +
-            "      AND ( " +
-            "        CASE WHEN dr.relation_source = 'internal' THEN " +
-            "            CASE WHEN dr.dataset_id = ? THEN CAST(dr.relateddataset_id AS VARCHAR) ELSE CAST(dr.dataset_id AS VARCHAR) END " +
-            "        ELSE dr.externalidentifier END " +
-            "        = " +
-            "        CASE WHEN dr2.relation_source = 'internal' THEN " +
-            "            CASE WHEN dr2.dataset_id = ? THEN CAST(dr2.relateddataset_id AS VARCHAR) ELSE CAST(dr2.dataset_id AS VARCHAR) END " +
-            "        ELSE dr2.externalidentifier END " +
-            "      ) " +
-            "    ORDER BY (CASE WHEN dr2.definitionpoint_id = ? THEN 0 ELSE 1 END) ASC, dr2.id ASC " +
-            "    LIMIT 1 " +
             " ) ";
 
     private static final String JOIN_DATASET_TYPES = 
@@ -111,14 +101,17 @@ public class SqlDirectDatasetRelationAlgorithm implements DatasetRelationAlgorit
             " (dr.relation_source IN (?)) ";
 
     private static final String ORDER_BY_REQUESTED_DATASET_FIRST =
-            " ORDER BY (CASE WHEN dv_def.dataset_id = ? THEN 0 ELSE 1 END) ASC, dr.id ASC ";
+            " ORDER BY cr.definition_point_priority ASC, dr.id ASC ";
 
     @SuppressWarnings("unchecked")
     @Override
     public List<DatasetRelation> getRelations(Dataset d, DatasetVersion v, List<String> relationTypeNames, List<String> datasetTypeNames, List<String> relationSources, Integer limit, Integer offset) {
         StringBuilder sql = new StringBuilder();
 
-        sql.append(GET_RELATIONS_QUERY_BASE);
+        sql.append(WITH_LATEST_RELEASED_VERSIONS)
+                .append(WITH_CANDIDATE_RELATIONS)
+                .append(WITH_CANONICAL_RELATIONS)
+                .append(GET_RELATIONS_QUERY_BASE);
 
         if (relationTypeNames != null && !relationTypeNames.isEmpty()) {
             sql.append(JOIN_RELATION_TYPES);
@@ -128,10 +121,7 @@ public class SqlDirectDatasetRelationAlgorithm implements DatasetRelationAlgorit
             sql.append(JOIN_DATASET_TYPES);
         }
 
-        sql.append(" WHERE ")
-                .append(WHERE_DATASET_OR_DATASET_VERSION_MATCHES)
-                .append(" AND ")
-                .append(WHERE_RELATION_IS_NOT_A_DUPLICATE);
+        sql.append(" WHERE 1 = 1 ");
 
         if (relationTypeNames != null && !relationTypeNames.isEmpty()) {
             sql.append(" AND ").append(WHERE_RELATION_TYPE_MATCHES.replace("(?)", 
@@ -152,28 +142,21 @@ public class SqlDirectDatasetRelationAlgorithm implements DatasetRelationAlgorit
 
         Query query = em.createNativeQuery(sql.toString(), DatasetRelation.class);
         int i = 1;
-        
+
+        // WITH_CANDIDATE_RELATIONS
+        query.setParameter(i++, v.getId());
+        query.setParameter(i++, d.getId());
+        query.setParameter(i++, d.getId());
+        query.setParameter(i++, d.getId());
+
+        // WITH_CANONICAL_RELATIONS
+        query.setParameter(i++, d.getId());
+        query.setParameter(i++, d.getId());
+
         if (datasetTypeNames != null && !datasetTypeNames.isEmpty()) {
             // JOIN_DATASET_TYPES
             query.setParameter(i++, d.getId());
         }
-
-        // WHERE_DATASET_OR_DATASET_VERSION_MATCHES
-        query.setParameter(i++, v.getId());
-        query.setParameter(i++, d.getId());
-        query.setParameter(i++, d.getId());
-        query.setParameter(i++, d.getId());
-
-        // WHERE_RELATION_IS_NOT_A_DUPLICATE
-        query.setParameter(i++, v.getId());
-        query.setParameter(i++, d.getId());
-        query.setParameter(i++, d.getId());
-        query.setParameter(i++, d.getId());
-        query.setParameter(i++, d.getId());
-        query.setParameter(i++, d.getId());
-        query.setParameter(i++, d.getId());
-        query.setParameter(i++, d.getId());
-        query.setParameter(i++, v.getId());
 
         if (relationTypeNames != null && !relationTypeNames.isEmpty()) {
             // WHERE_RELATION_TYPE_MATCHES
@@ -201,9 +184,6 @@ public class SqlDirectDatasetRelationAlgorithm implements DatasetRelationAlgorit
             }
         }
 
-        // ORDER_BY_REQUESTED_DATASET_FIRST
-        query.setParameter(i++, d.getId());
-
         return (List<DatasetRelation>) query.setMaxResults(limit)
                 .setFirstResult(offset)
                 .getResultList();
@@ -213,7 +193,9 @@ public class SqlDirectDatasetRelationAlgorithm implements DatasetRelationAlgorit
     public Long getTotalDatasetRelationCountFor(Dataset d, DatasetVersion v, List<String> relationTypeNames, List<String> datasetTypeNames, List<String> relationSources) {
         StringBuilder sql = new StringBuilder();
 
-        sql.append(GET_TOTAL_RELATION_COUNT_QUERY_BASE);
+        sql.append(WITH_LATEST_RELEASED_VERSIONS)
+                .append(WITH_CANDIDATE_RELATIONS)
+                .append(GET_TOTAL_RELATION_COUNT_QUERY_BASE);
 
         if (relationTypeNames != null && !relationTypeNames.isEmpty()) {
             sql.append(JOIN_RELATION_TYPES);
@@ -223,7 +205,7 @@ public class SqlDirectDatasetRelationAlgorithm implements DatasetRelationAlgorit
             sql.append(JOIN_DATASET_TYPES);
         }
 
-        sql.append(" WHERE ").append(WHERE_DATASET_OR_DATASET_VERSION_MATCHES);
+        sql.append(" WHERE 1 = 1 ");
 
         if (relationTypeNames != null && !relationTypeNames.isEmpty()) {
             sql.append(" AND ").append(WHERE_RELATION_TYPE_MATCHES.replace("(?)", 
@@ -242,6 +224,13 @@ public class SqlDirectDatasetRelationAlgorithm implements DatasetRelationAlgorit
 
         Query query = em.createNativeQuery(sql.toString());
         int i = 1;
+
+        // WITH_CANDIDATE_RELATIONS
+        query.setParameter(i++, v.getId());
+        query.setParameter(i++, d.getId());
+        query.setParameter(i++, d.getId());
+        query.setParameter(i++, d.getId());
+
         // GET_TOTAL_RELATION_COUNT_QUERY_BASE
         query.setParameter(i++, d.getId());
 
@@ -249,12 +238,6 @@ public class SqlDirectDatasetRelationAlgorithm implements DatasetRelationAlgorit
             // JOIN_DATASET_TYPES
             query.setParameter(i++, d.getId());
         }
-
-        // WHERE_DATASET_OR_DATASET_VERSION_MATCHES
-        query.setParameter(i++, v.getId());
-        query.setParameter(i++, d.getId());
-        query.setParameter(i++, d.getId());
-        query.setParameter(i++, d.getId());
 
         if (relationTypeNames != null && !relationTypeNames.isEmpty()) {
             // WHERE_RELATION_TYPE_MATCHES
--- tests/performance/relations/explain-list.sql
+++ tests/performance/relations/explain-list.sql
@@ -28,77 +28,64 @@ FROM relation_benchmark_target
 
 \echo Capturing list-query plan for dataset :target_dataset_id and version :target_version_id
 EXPLAIN (ANALYZE, BUFFERS)
+WITH latest_released_versions AS MATERIALIZED (
+    SELECT DISTINCT ON (dataset_id) id, dataset_id
+    FROM datasetversion
+    WHERE versionstate = 'RELEASED'
+    ORDER BY dataset_id, id DESC
+),
+candidate_relations AS (
+    SELECT dr.id, 0 AS definition_point_priority
+    FROM datasetrelation dr
+    WHERE dr.definitionpoint_id = :target_version_id
+    UNION ALL
+    SELECT dr.id, 1 AS definition_point_priority
+    FROM datasetrelation dr
+    JOIN latest_released_versions lrv ON dr.definitionpoint_id = lrv.id
+    WHERE lrv.dataset_id != :target_dataset_id
+      AND (dr.dataset_id = :target_dataset_id OR dr.relateddataset_id = :target_dataset_id)
+),
+normalized_candidate_relations AS (
+    SELECT cr.id, cr.definition_point_priority,
+           CASE WHEN dr.dataset_id = :target_dataset_id THEN dr.relationtype_id ELSE rt.inverse_id END AS normalized_relation_type_id,
+           CASE WHEN dr.relation_source = 'internal'
+                THEN CASE WHEN dr.dataset_id = :target_dataset_id THEN CAST(dr.relateddataset_id AS VARCHAR) ELSE CAST(dr.dataset_id AS VARCHAR) END
+                ELSE dr.externalidentifier
+           END AS normalized_related_dataset
+    FROM candidate_relations cr
+    JOIN datasetrelation dr ON cr.id = dr.id
+    LEFT JOIN datasetrelationtype rt ON dr.relationtype_id = rt.id
+),
+canonical_relations AS (
+    SELECT DISTINCT ON (normalized_relation_type_id, normalized_related_dataset) id, definition_point_priority
+    FROM normalized_candidate_relations
+    ORDER BY normalized_relation_type_id, normalized_related_dataset, definition_point_priority, id
+)
 SELECT dr.*
-FROM datasetrelation dr
-JOIN datasetversion dv_def ON dr.definitionpoint_id = dv_def.id
-WHERE (
-    dr.definitionpoint_id = :target_version_id
-    OR (
-        dv_def.dataset_id != :target_dataset_id
-        AND (dr.dataset_id = :target_dataset_id OR dr.relateddataset_id = :target_dataset_id)
-        AND dr.definitionpoint_id = (
-            SELECT dv.id
-            FROM datasetversion dv
-            WHERE dv.dataset_id = dv_def.dataset_id
-              AND dv.id = (
-                  SELECT MAX(dv2.id)
-                  FROM datasetversion dv2
-                  WHERE dv2.dataset_id = dv.dataset_id
-                    AND dv2.versionstate = 'RELEASED'
-              )
-        )
-    )
-)
-AND dr.id = (
-    SELECT dr2.id
-    FROM datasetrelation dr2
-    JOIN datasetversion dv_def2 ON dr2.definitionpoint_id = dv_def2.id
-    WHERE (
-        dr2.definitionpoint_id = :target_version_id
-        OR (
-            (dr2.dataset_id = :target_dataset_id OR (dr2.relation_source = 'internal' AND dr2.relateddataset_id = :target_dataset_id))
-            AND dv_def2.dataset_id != :target_dataset_id
-            AND dr2.definitionpoint_id = (
-                SELECT dv2.id
-                FROM datasetversion dv2
-                WHERE dv2.dataset_id = dv_def2.dataset_id
-                  AND dv2.id = (
-                      SELECT MAX(dv3.id)
-                      FROM datasetversion dv3
-                      WHERE dv3.dataset_id = dv2.dataset_id
-                        AND dv3.versionstate = 'RELEASED'
-                  )
-            )
-        )
-    )
-    AND (
-        CASE WHEN dr.dataset_id = :target_dataset_id THEN dr.relationtype_id
-             ELSE (SELECT rt.inverse_id FROM datasetrelationtype rt WHERE rt.id = dr.relationtype_id)
-        END
-        =
-        CASE WHEN dr2.dataset_id = :target_dataset_id THEN dr2.relationtype_id
-             ELSE (SELECT rt2.inverse_id FROM datasetrelationtype rt2 WHERE rt2.id = dr2.relationtype_id)
-        END
-    )
-    AND (
-        CASE WHEN dr.relation_source = 'internal' THEN
-            CASE WHEN dr.dataset_id = :target_dataset_id THEN CAST(dr.relateddataset_id AS VARCHAR) ELSE CAST(dr.dataset_id AS VARCHAR) END
-        ELSE dr.externalidentifier
-        END
-        =
-        CASE WHEN dr2.relation_source = 'internal' THEN
-            CASE WHEN dr2.dataset_id = :target_dataset_id THEN CAST(dr2.relateddataset_id AS VARCHAR) ELSE CAST(dr2.dataset_id AS VARCHAR) END
-        ELSE dr2.externalidentifier
-        END
-    )
-    ORDER BY (CASE WHEN dr2.definitionpoint_id = :target_version_id THEN 0 ELSE 1 END) ASC, dr2.id ASC
-    LIMIT 1
-)
-ORDER BY (CASE WHEN dv_def.dataset_id = :target_dataset_id THEN 0 ELSE 1 END) ASC, dr.id ASC
+FROM canonical_relations cr
+JOIN datasetrelation dr ON cr.id = dr.id
+ORDER BY cr.definition_point_priority ASC, dr.id ASC
 LIMIT 10 OFFSET 0;
 
 \echo Capturing total-count-query plan for dataset :target_dataset_id and version :target_version_id
 EXPLAIN (ANALYZE, BUFFERS)
+WITH latest_released_versions AS MATERIALIZED (
+    SELECT DISTINCT ON (dataset_id) id, dataset_id
+    FROM datasetversion
+    WHERE versionstate = 'RELEASED'
+    ORDER BY dataset_id, id DESC
+),
+candidate_relations AS (
+    SELECT dr.id, 0 AS definition_point_priority
+    FROM datasetrelation dr
+    WHERE dr.definitionpoint_id = :target_version_id
+    UNION ALL
+    SELECT dr.id, 1 AS definition_point_priority
+    FROM datasetrelation dr
+    JOIN latest_released_versions lrv ON dr.definitionpoint_id = lrv.id
+    WHERE lrv.dataset_id != :target_dataset_id
+      AND (dr.dataset_id = :target_dataset_id OR dr.relateddataset_id = :target_dataset_id)
+)
 SELECT COUNT(DISTINCT
     CASE
         WHEN dr.relation_source = 'internal' THEN
@@ -106,23 +93,5 @@ SELECT COUNT(DISTINCT
         ELSE dr.externalidentifier
     END
 )
-FROM datasetrelation dr
-JOIN datasetversion dv_def ON dr.definitionpoint_id = dv_def.id
-WHERE (
-    dr.definitionpoint_id = :target_version_id
-    OR (
-        dv_def.dataset_id != :target_dataset_id
-        AND (dr.dataset_id = :target_dataset_id OR dr.relateddataset_id = :target_dataset_id)
-        AND dr.definitionpoint_id = (
-            SELECT dv.id
-            FROM datasetversion dv
-            WHERE dv.dataset_id = dv_def.dataset_id
-              AND dv.id = (
-                  SELECT MAX(dv2.id)
-                  FROM datasetversion dv2
-                  WHERE dv2.dataset_id = dv.dataset_id
-                    AND dv2.versionstate = 'RELEASED'
-              )
-        )
-    )
-);
+FROM candidate_relations cr
+JOIN datasetrelation dr ON cr.id = dr.id;
